0c6fdbf45713df49166d9e74bfeebe374bcb0a19
yarden1233 <yarden28111@gmail.com>
2020-10-19 14:04:36 +0300

    final

--- app/src/main/java/com/example/andapp/MainActivity.java
+++ app/src/main/java/com/example/andapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.andapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int x=0;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.txtResult);
+    }
+
+    public void methodToDo(View view) {
+        x++;
+        String st=getString(R.string.textOnTextBox);
+        tv.setText(st+ " " + x);
     }
 }
